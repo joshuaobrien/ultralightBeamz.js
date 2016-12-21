17bacae1818ba47e6236e4701eb4a8cea3195a4f
tiget <jjobrien12@gmail.com>
2016-12-22 00:06:07 +1000

    added ring of static beamz

--- sketch.js
+++ sketch.js
@@ -1,9 +1,30 @@
+var ultralightBeamz = [];
 
 function setup() {
   createCanvas(800, 800);
+
+  for (var i = 0; i < 200; i++) {
+    ultralightBeamz[i] = new Beam();
+  }
 }
 
 function draw() {
   background(0, 0, 0);
 
+  for (var i = 0; i < ultralightBeamz.length; i++) {
+    ultralightBeamz[i].show();
+
+  }
+
+}
+
+function Beam() {
+  a = random(0, 2*Math.PI);
+  this.x = 50 * cos(a) + width/2;
+  this.y = 50 * sin(a) + width/2;
+
+  this.show = function() {
+    fill(165, 42, 42);
+    ellipse(this.x, this.y, 20, 20);
+  }
 }
