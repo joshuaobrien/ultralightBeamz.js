a64b2339259ba756d1be524b591e2a4cc4aca336
tiget <jjobrien12@gmail.com>
2016-12-22 00:16:56 +1000

    added the t h i r d d i m e n s i o n 👽

--- sketch.js
+++ sketch.js
@@ -13,18 +13,26 @@ function draw() {
 
   for (var i = 0; i < ultralightBeamz.length; i++) {
     ultralightBeamz[i].show();
-
+    ultralightBeamz[i].update();
   }
-
 }
 
 function Beam() {
   a = random(0, 2*Math.PI);
   this.x = 50 * cos(a) + width/2;
   this.y = 50 * sin(a) + width/2;
+  this.z = random(width);
 
   this.show = function() {
     fill(165, 42, 42);
-    ellipse(this.x, this.y, 20, 20);
+
+    var dx = map(this.x / this.z, 0, 1, 0, width);
+    var dy = map(this.y / this.z, 0, 1, 0, width);
+    
+    ellipse(dx, dy, 20, 20);
+  }
+
+  this.update = function() {
+    this.z = this.z < 1 ? width : this.z - 1;
   }
 }
