fa9557c79b039fb6ed638a78977926b7f43f4e47
tiget <jjobrien12@gmail.com>
2016-12-22 17:25:50 +1000

    Added ye to centre

--- sketch.js
+++ sketch.js
@@ -11,6 +11,7 @@ function setup() {
 function draw() {
   background(0, 0, 0);
 
+
   for (var i = 0; i < ultralightBeamz.length; i++) {
     ultralightBeamz[i].show();
     ultralightBeamz[i].update();
@@ -19,16 +20,16 @@ function draw() {
 
 function Beam() {
   a = random(0, 2*Math.PI);
-  this.x = 50 * cos(a) + width/2;
-  this.y = 50 * sin(a) + width/2;
+  this.x = 50 * cos(a);
+  this.y = 50 * sin(a);
   this.z = random(width);
 
   this.show = function() {
     fill(165, 42, 42);
 
-    var dx = map(this.x / this.z, 0, 1, 0, width);
-    var dy = map(this.y / this.z, 0, 1, 0, width);
-    
+    var dx = map(this.x / this.z, 0, 1, 0, width) + width/2;
+    var dy = map(this.y / this.z, 0, 1, 0, width) + width/2;
+
     ellipse(dx, dy, 20, 20);
   }
 
